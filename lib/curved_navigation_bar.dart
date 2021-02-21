98a55b9664130e09b9ce757ee388f1a02c6989d9
kelvin6365 <kelvin6365@gmail.com>
2021-02-22 00:12:50 +0800

    save

--- lib/curved_navigation_bar.dart
+++ lib/curved_navigation_bar.dart
@@ -16,20 +16,22 @@ class CurvedNavigationBar extends StatefulWidget {
   final Curve animationCurve;
   final Duration animationDuration;
   final double height;
+  final BoxDecoration boxDecoration;
 
-  CurvedNavigationBar({
-    Key key,
-    @required this.items,
-    this.index = 0,
-    this.color = Colors.white,
-    this.buttonBackgroundColor,
-    this.backgroundColor = Colors.blueAccent,
-    this.onTap,
-    _LetIndexPage letIndexChange,
-    this.animationCurve = Curves.easeOut,
-    this.animationDuration = const Duration(milliseconds: 600),
-    this.height = 75.0,
-  })  : letIndexChange = letIndexChange ?? ((_) => true),
+  CurvedNavigationBar(
+      {Key key,
+      @required this.items,
+      this.index = 0,
+      this.color = Colors.white,
+      this.buttonBackgroundColor,
+      this.backgroundColor = Colors.blueAccent,
+      this.onTap,
+      _LetIndexPage letIndexChange,
+      this.animationCurve = Curves.easeOut,
+      this.animationDuration = const Duration(milliseconds: 600),
+      this.height = 75.0,
+      this.boxDecoration})
+      : letIndexChange = letIndexChange ?? ((_) => true),
         assert(items != null),
         assert(items.length >= 1),
         assert(0 <= index && index < items.length),
@@ -118,10 +120,12 @@ class CurvedNavigationBarState extends State<CurvedNavigationBar>
                 child: Material(
                   color: widget.buttonBackgroundColor ?? widget.color,
                   type: MaterialType.circle,
-                  child: Padding(
+                  child: Container(
+                      child: Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: _icon,
-                  ),
+                    decoration: widget.boxDecoration,
+                  )),
                 ),
               ),
             ),
